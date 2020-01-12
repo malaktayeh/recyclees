289dd7c84fa6c912a37ac43d220190965c9f4dbe
Malak Tayeh <malaktayeh@outlook.com>
2020-01-12 18:20:44 -0500

    implemented auth0

--- app.py
+++ app.py
@@ -2,7 +2,8 @@ import os
 from models import setup_db
 from flask import Flask, request, abort, jsonify
 from flask_sqlalchemy import SQLAlchemy
-from flask_cors import CORS
+from flask_cors import CORS, cross_origin
+from server import AuthError, requires_auth, requires_scope
 
 app = Flask(__name__)
 
@@ -16,6 +17,34 @@ def create_app(test_config=None):
     @app.route('/')
     def hello_world():
         return 'Hello, World!'
+
+    # This doesn't need authentication
+    @app.route("/api/public")
+    @cross_origin(headers=["Content-Type", "Authorization"])
+    def public():
+        response = "Hello from a public endpoint! You don't need to be authenticated to see this."
+        return jsonify(message=response)
+
+    # This needs authentication
+    @app.route("/api/private")
+    @cross_origin(headers=["Content-Type", "Authorization"])
+    @requires_auth
+    def private():
+        response = "Hello from a private endpoint! You need to be authenticated to see this."
+        return jsonify(message=response)
+
+    # This needs authorization
+    @app.route("/api/private-scoped")
+    @cross_origin(headers=["Content-Type", "Authorization"])
+    @requires_auth
+    def private_scoped():
+        if requires_scope("read:messages"):
+            response = "Hello from a private endpoint! You need to be authenticated and have a scope of read:messages to see this."
+            return jsonify(message=response)
+        raise AuthError({
+            "code": "Unauthorized",
+            "description": "You don't have access to this resource"
+        }, 403)
 
     return app
 
