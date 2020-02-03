e52747dd55bd68f769276c441efb4e6d3f6f2066
Malak Tayeh <malaktayeh@outlook.com>
2020-02-02 21:31:59 -0500

    added dependencies for code formatting  w/ prettier

--- recyclees/src/components/navbar.js
+++ recyclees/src/components/navbar.js
@@ -1,36 +1,40 @@
-import React from 'react';
-import Logo from '../assets/logo.png';
-import { Navbar, Nav, NavDropdown, Button } from 'react-bootstrap';
+import React from "react";
+import Logo from "../assets/logo.png";
+import { Navbar, Nav, NavDropdown, Button } from "react-bootstrap";
 
 function Navigation() {
-    return(
-        <Navbar bg="light" expand="lg" fixed="top" >
-            <Navbar.Brand href="#home">
-            <img
-                alt=""
-                src={Logo}
-                width="30"
-                height="30"
-                className="d-inline-block align-top"
-            />{' '}
-            recyclees
-            </Navbar.Brand>
-            <Navbar.Toggle aria-controls="basic-navbar-nav" />
-            <Navbar.Collapse id="basic-navbar-nav">
-                <Nav className="mr-auto">
-                <Nav.Link href="#link">Link</Nav.Link>
-                <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                    <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                    <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-                    <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                    <NavDropdown.Divider />
-                    <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-                </NavDropdown>
-                </Nav>
-                <Button variant="outline-primary">Sign in</Button>
-            </Navbar.Collapse>
-        </Navbar>
-    )
+  return (
+    <Navbar bg="light" expand="lg" fixed="top">
+      <Navbar.Brand href="#home">
+        <img
+          alt=""
+          src={Logo}
+          width="30"
+          height="30"
+          className="d-inline-block align-top"
+        />{" "}
+        recyclees
+      </Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="mr-auto">
+          <Nav.Link href="#link">Link</Nav.Link>
+          <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.2">
+              Another action
+            </NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+            <NavDropdown.Divider />
+            <NavDropdown.Item href="#action/3.4">
+              Separated link
+            </NavDropdown.Item>
+          </NavDropdown>
+        </Nav>
+        <Button variant="outline-primary">Sign in</Button>
+      </Navbar.Collapse>
+    </Navbar>
+  );
 }
 
 export default Navigation;
